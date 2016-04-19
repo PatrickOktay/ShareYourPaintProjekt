d8967f4a2701503b064774b176f70e5d34501a91
CatiMira <CatiiMira@gmail.com>
2016-04-19 11:38:35 +0200

    Grundgerüst

---
+++ view/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+  <title>ShareYourPaint</title>
+  <link rel="stylesheet" type="text/css" href="/view/css/style.css">
+</head>
+<body>
+<img src="pictures/ShareYourPaint.PNG" alt="ShareYourPaint">
+<form id="menu">
+  <a href="index.php">Home</a>
+  <a href="me.php">My profile</a>
+  <a href="all.php">All pictures</a>
+  <a href="best.php">The best rated pictures</a>
+  <a href="others.php">Other users profiles</a>
+</form>
+<div id="main">
