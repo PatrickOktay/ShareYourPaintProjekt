4a5c633dca622c916d1891b952a3b75ce6c19b2f
CatiMira <CatiiMira@gmail.com>
2016-04-20 11:31:38 +0200

    Style

---
+++ model/PictureModel.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once 'lib/Model.php';
+
+/**
+ * Das UserModel ist zuständig für alle Zugriffe auf die Tabelle "user".
+ *
+ * Die Ausführliche Dokumentation zu Models findest du in der Model Klasse.
+ */
+class PictureModel extends Model
+{
+    /**
+     * Diese Variable wird von der Klasse Model verwendet, um generische
+     * Funktionen zur Verfügung zu stellen.
+     */
+    protected $tableName = 'pictures';
+
+    /**
+     * Erstellt einen neuen benutzer mit den gegebenen Werten.
+     *
+     * Das Passwort wird vor dem ausführen des Queries noch mit dem SHA1
+     *  Algorythmus gehashed.
+     *
+     * @param $firstName Wert für die Spalte firstName
+     * @param $lastName Wert für die Spalte lastName
+     * @param $email Wert für die Spalte email
+     * @param $password Wert für die Spalte password
+     *
+     * @throws Exception falls das Ausführen des Statements fehlschlägt
+     */
+    public function create($username, $password, $description)
+    {
+        $password = sha1($password);
+
+        $query = "INSERT INTO $this->tableName (username, password, description) VALUES (?, ?, ?)";
+
+        $statement = ConnectionHandler::getConnection()->prepare($query);
+        $statement->bind_param('sss', $username, $password, $description);
+
+        if (!$statement->execute()) {
+            throw new Exception($statement->error);
+        }
+    }
+
+    public function login($username, $password)
+    {
+        $password = sha1($password);
+
+        $query = "SELECT id FROM $this->tableName WHERE username =? AND password =?";
+
+        $statement = ConnectionHandler::getConnection()->prepare($query);
+        $statement->bind_param('ss', $username, $password);
+        $statement->execute();
+
+        $result = $statement->get_result();
+        $row = $result->fetch_object();
+        
+        if (!$result) {
+            echo "You are not registred yet!";
+        }
+        else{
+            echo "Welcome!";
+        }
+        $result->close();
+    }
+}
--- view/user_login.php
+++ view/user_login.php
@@ -1,8 +1,8 @@
 <div class="login">
 	<form id="login" name="login" method="post" action="/User/loginUser">
-		<input id="loginname" name="loginname" type="text" placeholder="Username">
+		<input id="loginname" name="loginname" type="text" placeholder="Username" required="true">
 		<br>
-		<input type="password" id="logpassword" name="logpassword" placeholder="Password">
+		<input type="password" id="logpassword" name="logpassword" placeholder="Password" required="true">
 		<br><br>
 		<button type="submit">Log In</button>
 	</form>
